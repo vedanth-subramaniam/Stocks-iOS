99f41d7356c4106d43119ebe86b4eb9b89442987
Vedanth Subramaniam <vedanth.1113@gmail.com>
2024-04-13 22:04:36 -0700

    Init repo

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Vedanth Subramaniam on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
